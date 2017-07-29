75aff7156688af098070030dde16d4f03ad2c9f1
Mike Doubintchik <mike@allurewebsolutions.com>
2017-07-29 00:30:19 -0400

    Add filter for VC shortcakes

--- public/class-wp-post-modal-public.php
+++ public/class-wp-post-modal-public.php
@@ -20,152 +20,155 @@
  * @subpackage WP_Post_Modal/public
  * @author     Allure Web Solutions <info@allurewebsolutions.com>
  */
-class WP_Post_Modal_Public
-{
+class WP_Post_Modal_Public {
 
-    /**
-     * The ID of this plugin.
-     *
-     * @since    1.0.0
-     * @access   private
-     * @var      string $plugin_name The ID of this plugin.
-     */
-    private $plugin_name;
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $plugin_name The ID of this plugin.
+	 */
+	private $plugin_name;
 
-    /**
-     * The version of this plugin.
-     *
-     * @since    1.0.0
-     * @access   private
-     * @var      string $version The current version of this plugin.
-     */
-    private $version;
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $version The current version of this plugin.
+	 */
+	private $version;
 
-    /**
-     * Initialize the class and set its properties.
-     *
-     * @since    1.0.0
-     * @param      string $plugin_name The name of the plugin.
-     * @param      string $version The version of this plugin.
-     */
-    public function __construct($plugin_name, $version)
-    {
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 *
+	 * @param      string $plugin_name The name of the plugin.
+	 * @param      string $version The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
 
-        $this->plugin_name = $plugin_name;
-        $this->version = $version;
+		$this->plugin_name = $plugin_name;
+		$this->version     = $version;
 
-    }
+	}
 
-    /**
-     * Register the stylesheets for the public-facing side of the site.
-     *
-     * @since    1.0.0
-     */
-    public function enqueue_styles()
-    {
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
 
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in WP_Post_Modal_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The WP_Post_Modal_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in WP_Post_Modal_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The WP_Post_Modal_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
 
-        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/wp-post-modal-public.css', array(), $this->version, 'all');
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-post-modal-public.css', array(), $this->version, 'all' );
 
-    }
+	}
 
-    /**
-     * Register the JavaScript for the public-facing side of the site.
-     *
-     * @since    1.0.0
-     */
-    public function enqueue_scripts()
-    {
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
 
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in WP_Post_Modal_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The WP_Post_Modal_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in WP_Post_Modal_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The WP_Post_Modal_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
 
-        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/wp-post-modal-public.js', array('jquery'), $this->version, false);
-        wp_localize_script($this->plugin_name, 'fromPHP', array(
-            'pluginUrl' => plugin_dir_url(__FILE__),
-            'breakpoint' => (get_option('wp_post_modal_breakpoint') != '' ? get_option('wp_post_modal_breakpoint') : '768'),
-            'styled' => get_option('wp_post_modal_styling'),
-            'ajax_url' => admin_url('admin-ajax.php')
-        ));
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-post-modal-public.js', array( 'jquery' ), $this->version, false );
+		wp_localize_script( $this->plugin_name, 'fromPHP', array(
+			'pluginUrl'  => plugin_dir_url( __FILE__ ),
+			'breakpoint' => ( get_option( 'wp_post_modal_breakpoint' ) != '' ? get_option( 'wp_post_modal_breakpoint' ) : '768' ),
+			'styled'     => get_option( 'wp_post_modal_styling' ),
+			'ajax_url'   => admin_url( 'admin-ajax.php' )
+		) );
 
-    }
+	}
 
-    /**
-     * Display Modal wrapper
-     *
-     * @since   1.0.0
-     */
-    public function modal_wrapper()
-    {
-        $styled = (get_option('wp_post_modal_styling') === '1' ? 'styled' : '');
+	/**
+	 * Display Modal wrapper
+	 *
+	 * @since   1.0.0
+	 */
+	public function modal_wrapper() {
+		$styled = ( get_option( 'wp_post_modal_styling' ) === '1' ? 'styled' : '' );
 
-        $close = (get_option('wp_post_modal_close') != '' ? get_option('wp_post_modal_close') : '×');
+		$close = ( get_option( 'wp_post_modal_close' ) != '' ? get_option( 'wp_post_modal_close' ) : '×' );
 
-        $HTML = '';
-        $HTML .= '<div class="modal-wrapper ' . $styled . '">';
-        $HTML .= '<div class="modal">';
-        $HTML .= '<div class="close-modal">' . $close . '</div>';
-        $HTML .= '<div id="modal-content"></div>';
-        $HTML .= '</div>';
-        $HTML .= '</div>';
+		$HTML = '';
+		$HTML .= '<div class="modal-wrapper ' . $styled . '">';
+		$HTML .= '<div class="modal">';
+		$HTML .= '<div class="close-modal">' . $close . '</div>';
+		$HTML .= '<div id="modal-content"></div>';
+		$HTML .= '</div>';
+		$HTML .= '</div>';
 
-        echo $HTML;
+		echo $HTML;
 
-    }
+	}
 
-    public function any_post_api_route()
-    {
+	/**
+	 * Register API Route: Query Any Post Type
+	 */
+	public function any_post_api_route() {
 
-        register_rest_route($this->plugin_name . '/v1', '/any-post-type/', array(
-            'methods' => 'GET',
-            'callback' => array($this, 'get_content_by_slug'),
-            'args' => array(
-                'slug' => array(
-                    'required' => false
-                )
-            )
-        ));
+		register_rest_route( $this->plugin_name . '/v1', '/any-post-type/', array(
+			'methods'  => 'GET',
+			'callback' => array( $this, 'get_content_by_slug' ),
+			'args'     => array(
+				'slug' => array(
+					'required' => false
+				)
+			)
+		) );
 
-    }
+	}
 
-    /**
-     *
-     * Get content by slug
-     *
-     * @param WP_REST_Request $request
-     * @return WP_REST_Response
-     */
-    public function get_content_by_slug(WP_REST_Request $request)
-    {
+	/**
+	 *
+	 * Get content by slug
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function get_content_by_slug( WP_REST_Request $request ) {
+		WPBMap::addAllMappedShortcodes();
 
-        // get slug from request
-        $slug = $request['slug'];
+		// get slug from request
+		$slug = $request['slug'];
 
-        // get title by slug
-        $return = get_page_by_path($slug, ARRAY_A, array('page', 'post'));
+		// get title by slug
+		$return = get_page_by_path( $slug, ARRAY_A, array( 'page', 'post' ) );
 
-        $response = new WP_REST_Response($return);
-        return $response;
+		// render shortcodes from Visual Composer
+		$return['post_content'] = apply_filters( 'the_content', $return['post_content'] );
 
-    }
+		$response = new WP_REST_Response( $return );
+
+		return $response;
+
+	}
 }
