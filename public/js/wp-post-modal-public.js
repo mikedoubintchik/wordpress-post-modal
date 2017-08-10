da669b5e18518ecdf3a32876ce003e3389c3a68d
Mike Doubintchik <mike@allurewebsolutions.com>
2017-08-10 13:50:35 -0400

    Remove php proxy and use ajax instead

--- public/js/wp-post-modal-public.js
+++ public/js/wp-post-modal-public.js
@@ -90,7 +90,21 @@
 
                     // Load content from external
                     if ($this.isExternal()) {
-                        modalContent.load(fromPHP.pluginUrl + '/includes/proxy.php?url=' + encodeURI(postLink) + dataDivID);
+
+                        $.ajaxPrefilter(function (options) {
+                            if (options.crossDomain && jQuery.support.cors) {
+                                var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+                                options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+                                //options.url = "http://cors.corsproxy.io/url=" + options.url;
+                            }
+                        });
+
+                        $.get(
+                            postLink,
+                            function (response) {
+                                var html = $(response);
+                                modalContent.html($(html).find(dataDivID).html());
+                            });
                     }
                     // Load content from internal
                     else {
